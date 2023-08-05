cdc00b0cde70f1e6bbc7578e6c98f97fe0d7af23
Eduardo Aire <hola@eduairet.com>
2023-08-04 19:01:17 -0600

    Basics notes 1

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.Write("Hello, World!"); // No line break
-Console.Write(args[0]); // Argument 1 from the CLI
+﻿//// 1 byte is made up of 8 bits 00000000 - these bits can be used to store a number as follows
+// //// Each bit can be worth 0 or 1 of the value it is placed in
+// ////// From the right we start with a value of 1 and double for each digit to the left
+// //// 00000000 = 0 bits
+// //// 00000001 = 1 bit
+// //// 00000010 = 2 bits
+// //// 00000011 = 3 bits
+// //// 00000100 = 4 bits
+// //// 00000101 = 5 bits
+// //// 00000110 = 6 bits
+// //// 00000111 = 7 bits
+// //// 00001000 = 8 bits
+
+// 1 byte (8 bit) unsigned, where signed means it can be negative
+byte myByte = 255;
+byte mySecondByte = 0;
+
+// 1 byte (8 bit) signed, where signed means it can be negative
+sbyte mySbyte = 127;
+sbyte mySecondSbyte = -128;
+
+
+// 2 byte (16 bit) unsigned, where signed means it can be negative
+ushort myUshort = 65535;
+
+// 2 byte (16 bit) signed, where signed means it can be negative
+short myShort = -32768;
+
+// 4 byte (32 bit) signed, where signed means it can be negative
+int myInt = 2147483647;
+int mySecondInt = -2147483648;
+
+// 8 byte (64 bit) signed, where signed means it can be negative
+long myLong = -9223372036854775808;
+
+
+// 4 byte (32 bit) floating point number
+float myFloat = 0.751f;
+float mySecondFloat = 0.75f;
+
+// 8 byte (64 bit) floating point number
+double myDouble = 0.751;
+double mySecondDouble = 0.75d;
+
+// 16 byte (128 bit) floating point number
+decimal myDecimal = 0.751m;
+decimal mySecondDecimal = 0.75m;
+
+// Console.WriteLine(myFloat - mySecondFloat);
+// Console.WriteLine(myDouble - mySecondDouble);
+// Console.WriteLine(myDecimal - mySecondDecimal);
+
+
+
+string myString = "Hello World";
+// Console.WriteLine(myString);
+string myStringWithSymbols = "!@#$@^$%%^&(&%^*__)+%^@##$!@%123589071340698ughedfaoig137";
+// Console.WriteLine(myStringWithSymbols);
+
+bool myBool = true;
--- README.md
+++ README.md
@@ -44,6 +44,35 @@
 
 -   Variable types
 
+    -   Bytes can be numbers in a range of 256 elements (8 bits)
+
+        ```CSHARP
+        // bit - 0 or 1
+        // byte - 8 bytes
+        // 00000000 = 0
+        // 00000001 = 1
+        // 00000010 = 2
+        // 00000011 = 3
+        // 00000100 = 4
+        // 00000101 = 5
+        // 00000110 = 6
+        // 00000111 = 7
+        // 00001000 = 8
+        // and so on...
+        ```
+
+        -   Signed bytes (just positive between 0 - 255)
+        -   Unsigned bytes (positive and negative between -128 - 127)
+
+            ```CSHARP
+            // Signed
+            byte myByte = 255;
+            byte mySecondByte = 0;
+            sbyte mySbyte = 127;
+            // Unsigned
+            sbyte mySecondSbyte = -128;
+            ```
+
 [^1]:
     Notice that previous versions of .NET have these functions inside a class and they use a different syntax
 
