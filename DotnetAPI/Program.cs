56363d763bf7460e128bcaa461c85037fdd46617
Eduardo Aire <hola@eduairet.com>
2023-11-22 20:42:45 -0600

    Hyphened Controller URL

--- DotnetAPI/Program.cs
+++ DotnetAPI/Program.cs
@@ -1,8 +1,14 @@
+using DotnetAPI.Utils;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+    // Routes with hyphens
+    options.Conventions.Add(new RouteTokenTransformerConvention(new SlugifyParameterTransformer()))
+);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ DotnetAPI/Utils/SlugifyParameterTransformer.cs
@@ -0,0 +1,17 @@
+using System.Text.RegularExpressions;
+
+namespace DotnetAPI.Utils;
+
+public partial class SlugifyParameterTransformer : IOutboundParameterTransformer
+{
+    public string? TransformOutbound(object? value)
+    {
+        if (value == null) { return null; }
+        string? str = value.ToString();
+        if (string.IsNullOrEmpty(str)) { return null; }
+        return MyRegex().Replace(str, "$1-$2").ToLower();
+    }
+
+    [GeneratedRegex("([a-z])([A-Z])")]
+    private static partial Regex MyRegex();
+}
